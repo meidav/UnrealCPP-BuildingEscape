52e57d90204c5593d03e0778d44242077d1e11dd
Arbel Meidav <github@meidav.com>
2019-02-12 13:43:53 -0800

    UECPP 82 Getting Player Viewpoint

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -2,6 +2,8 @@
 
 #include "Grabber.h"
 
+#define OUT
+
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
@@ -18,7 +20,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp,Warning,TEXT("Grabber Reporting for Duty!!!"));
+	UE_LOG(LogTemp,Warning,TEXT("Grabber reporting for duty!!!"));
 }
 
 
@@ -27,6 +29,21 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// get player view point
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation
+    );
+    // TODO: logout to test
+    UE_LOG(LogTemp,Warning,TEXT("Location: %s, Rotation: %s"),
+           *PlayerViewPointLocation.ToString(),
+           *PlayerViewPointRotation.ToString()
+    );
+
+    // ray-cast out to reach distance
+    
+    // see what we hit
 }
 
