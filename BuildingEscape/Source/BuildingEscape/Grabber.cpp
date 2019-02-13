0c52ceeee2398e62197b8d37fd6c2a3cad815dd0
Arbel Meidav <github@meidav.com>
2019-02-13 12:33:06 -0800

    UECPP 92 Using Physics Handles

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -29,25 +29,42 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
     
+    /// get player view point
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+                                                               OUT PlayerViewPointLocation,
+                                                               OUT PlayerViewPointRotation
+                                                               );
+    
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+    
     // if the physics handle is attached
+    if (PhysicsHandle->GrabbedComponent) {
         // move the object we're holding
-
-	
+        PhysicsHandle->SetTargetLocation(LineTraceEnd);
+    }
+    
 }
 
 void UGrabber::Grab()
 {
     UE_LOG(LogTemp,Warning,TEXT("Grabbed by %s"), *GetOwner()->GetName());
     /// LINE TRACE and reach any actors with physics body collision channel set
-    GetFirstPhysicsBodyInReach();
+    auto HitResult = GetFirstPhysicsBodyInReach();
+    auto ComponentToGrab = HitResult.GetComponent();
+    auto ActorHit = HitResult.GetActor();
+    
     /// if we hit something then attach a physics handle
-        /// TODO: attach physics handle
+    if (ActorHit) {
+        PhysicsHandle->GrabComponent(ComponentToGrab, NAME_None, ComponentToGrab->GetOwner()->GetActorLocation(), true);
+    }
 }
 
 void UGrabber::Release()
 {
     UE_LOG(LogTemp,Warning,TEXT("Released by %s"), *GetOwner()->GetName());
-    /// TODO: release physics handle
+    PhysicsHandle->ReleaseComponent();
 }
 
 /// look for attached physics handle
